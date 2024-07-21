90fbe73732a1911e7547c382d2bda215915f818f
Nevo <bitonnevo@gmail.com>
2024-07-21 15:52:07 +0300

    asd

--- src/context/userProvider.tsx
+++ src/context/userProvider.tsx
@@ -1,5 +1,6 @@
 import { api } from "../lib/utils";
 
+
 import React, { createContext, useState, useEffect, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -9,6 +10,7 @@ export interface User {
   email: string;
   firstName: string;
   lastName: string;
+
   image?: string;
 }
 
