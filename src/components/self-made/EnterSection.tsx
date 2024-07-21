a6d62fb81cbfffd34bd1bdb214757253c18bda43
RonGamarnik <rgam2002@gmail.com>
2024-07-21 09:17:14 +0300

    21/07 init

--- src/components/self-made/EnterSection.tsx
+++ src/components/self-made/EnterSection.tsx
@@ -4,11 +4,11 @@ import { Button } from '../ui/button'
 function EnterSection() {
     const navigate = useNavigate();
   return (
-      <section className="py-20 text-center bg-hero-pattern bg-cover bg-center">
+      <section className="py-20 text-center bg-hero-pattern bg-cover bg-center text-black">
           <div className="container mx-auto px-4">
-            <h2 className="text-5xl  text-black  font-extrabold mb-6 text-primary-foreground shadow-text">Your Trusted Review Companion</h2>
-            <p className="text-xl  text-black mb-8 max-w-2xl mx-auto text-primary-foreground shadow-text">Discover, rate, and share your experiences with RateMaster - the ultimate platform for honest reviews.</p>
-            <Button onClick={()=>navigate("/SignIn")} size="lg" className="bg-primary hover:bg-primary/90 text-primary-foreground">Get Started</Button>
+            <h2 className="text-5xl text-black  font-extrabold mb-6  shadow-text">Your Trusted Review Companion</h2>
+            <p className="text-xl text-black mb-8 max-w-2xl mx-auto  shadow-text">Discover, rate, and share your experiences with RateMaster - the ultimate platform for honest reviews.</p>
+            <Button onClick={()=>navigate("/SignIn")} size="lg" className="bg-primary hover:bg-primary/90 ">Get Started</Button>
           </div>
         </section>
   )
