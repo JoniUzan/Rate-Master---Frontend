a8c24491d234a6b27dcbc674f5d62cc5afd6757d
Nevo <bitonnevo@gmail.com>
2024-07-23 18:18:46 +0300

    i

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import PrivacyPolicy from "./pages/privacyPolicy";
